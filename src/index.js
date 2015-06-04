599c589a98cca6f35f01d329e20a3d77cb0e01b0
Andy Baird <Andy.Baird@icloud.com>
2015-06-03 23:08:40 -0400

    Weather API is working, need to fix it so it only displays when button is clicked

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 $(function(){
-
 	$('.side-bar-btn').click(function showArrow (){
 		$(this)
 		.siblings(".arrow_box")
 		.css({visibility: 'visible'})
 	});
 
+$('.btn-weather').click(getWeather(27701));
 
+function getWeather(zipCode) {
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us')
+    .done(function (data) {
+    	$('.active-title').html("Current Weather");
+    	$('.active-content').html(data.weather[0].description + ' in ' + data.name + ' at the moment');
+  });
+}
 
 
 
 });
 
+
+
+
+//function getWeather(){
+//	var currentWeather = '';
+//	 $.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=27701,us')
+//	  .done(function (data) {
+//	      var weatherDescription = data.weather[0].description;
+//		  var weatherCityName = data.name;
+//		  var currentWeather = weatherCityName + ', ' + weatherDescription; 
+//		  console.log(currentWeather);
+//		  return currentWeather; 	   
+//	  });
+//	
+//
+//}
